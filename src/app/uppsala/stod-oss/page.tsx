df242f685648c7dc9b9d785c46dc2cdf0e6d73a2
Oskar Thornblad <oskar.thornblad@gmail.com>
2023-12-10 23:35:42 +0100

    Add initial uppsala stuff

---
+++ src/app/uppsala/stod-oss/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import Content from './content.mdx'
+import UppsalaWrapper from '@/components/uppsala-wrapper';
+
+export default function Page() {
+  return <UppsalaWrapper><Content/></UppsalaWrapper>
+}
---
+++ src/components/uppsala-wrapper.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { LocalWrapper } from "./local-wrapper";
+import React from "react";
+import { usePathname } from 'next/navigation';
+
+export default function GoteborgWrapper({children}: { children: React.ReactNode; }) {
+  const pathname = usePathname();
+
+  return (
+    <LocalWrapper
+      title="Uppsala"
+      titleLink="/uppsala"
+      socials={[
+        ["Facebook", "https://www.facebook.com/ingenillegal.uppsala"],
+      ]}
+      links={[
+        ["Stöd oss", "/uppsala/stod-oss"],
+        ["Engagera dig", "/uppsala/engagera-dig"],
+        ["Om oss", "/uppsala/om-oss"],
+        ["English", "/uppsala/english"],
+      ]}
+      pathname={pathname}
+    >
+      {children}
+    </LocalWrapper>
+  );
+}
