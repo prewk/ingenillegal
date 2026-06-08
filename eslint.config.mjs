1603877d3a92b52b8ad216d1382fb4729524aafd
Oskar Thornblad <oskar.thornblad@gmail.com>
2026-06-08 20:48:29 +0200

    Upgrade next to 16

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import prettier from "eslint-config-prettier";
+
+const config = [...nextCoreWebVitals, prettier];
+export default config;
