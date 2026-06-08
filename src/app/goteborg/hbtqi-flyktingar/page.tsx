faee15ad0307c75465fd0849dada8ada1e34db52
Oskar Thornblad <oskar.thornblad@gmail.com>
2026-06-08 21:00:22 +0200

    Gbg - Flyktingbarnmottagning & HBTQI-flyktingar

---
+++ src/app/goteborg/hbtqi-flyktingar/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Content from "./content.mdx";
+import GoteborgWrapper from "@/components/goteborg-wrapper";
+
+export default function Page() {
+  return (
+    <GoteborgWrapper>
+      <Content />
+    </GoteborgWrapper>
+  );
+}
